8ed56be58dc3503f59f9830ee158160f9629c3f5
Katya <kittyserenakat@gmail.com>
2022-12-17 23:52:10 -0500

    Add orders list

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,30 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
-// TODO: Implement the /orders handlers needed to make the tests pass
+function create (req, res, next){
+    return "yes"
+}
+
+function read (req, res, next){
+    return "yes"
+}
+
+function update (req, res, next){
+    return "yes"
+}
+
+function destroy (req, res, next){
+    return "yes"
+}
+
+function list (req, res, next){
+    return res.json({ data: orders })
+}
+
+module.exports = {
+    create: [create],
+    read: [read],
+    update: [update],
+    delete: [destroy],
+    list
+}
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
+const controller = require("./orders.controller");
 
-// TODO: Implement the /orders routes needed to make the tests pass
+router
+    .route("/")
+    .get(controller.list)
 
 module.exports = router;
