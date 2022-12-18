604a499812566299c8ac534b341696a2d0087839
Katya <kittyserenakat@gmail.com>
2022-12-18 01:11:06 -0500

    Add orders bodyHasData and create

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,8 +6,58 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
+function bodyHasData(property){
+    return function (req, res, next){
+        const { data } = req.body;
+        const currProp = data[property];
+
+        if(property === "dishes"){
+            if(!Array.isArray(currProp)){
+                return next({
+                    status: 400,
+                    message: `Missing ${property}. Please fill in all the fields.`
+                })
+            }
+            if(currProp.length === 0){
+                return next({
+                    status: 400,
+                    message: `Missing ${property}. Please fill in all the fields.`
+                })
+            }
+            const index = currProp.findIndex( order => {
+                return !order.quantity || order.quantity <= 0 || typeof order.quantity !== "number"
+            })
+            if(index > -1){
+                return next({
+                    status: 400,
+                    message: `Dish ${index} must have a quantity that is an integer greater than 0`
+                })
+            }
+        }
+        if( !currProp ||  currProp === ""){
+            return next({
+                status: 400,
+                message: `Missing ${property}. Please fill in all the fields.`
+            })
+        }
+        return next();
+    }
+}
+
 function create (req, res, next){
-    return "yes"
+    const { data } = req.body;
+    const id = nextId();
+    const newOrder = {
+        ...data,
+        id: id
+    }
+    orders.push(newOrder);
+    res.status(201).json({ data: newOrder })
+}
+
+function orderExists (req, res, next){
+    console.log(req.body)
+    return next();
 }
 
 function read (req, res, next){
@@ -27,8 +77,13 @@ function list (req, res, next){
 }
 
 module.exports = {
-    create: [create],
-    read: [read],
+    create: [
+        bodyHasData("deliverTo"),
+        bodyHasData("mobileNumber"),
+        bodyHasData("dishes"),
+        create
+    ],
+    read: [orderExists, read],
     update: [update],
     delete: [destroy],
     list
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -4,5 +4,6 @@ const controller = require("./orders.controller");
 router
     .route("/")
     .get(controller.list)
+    .post(controller.create)
 
 module.exports = router;
